8dea79550827f2cceba7ddf27f7b5ae302366107
VilmaoWebServices <webservices00@hotmail.com>
2025-08-18 10:50:17 +0200

    2

---
+++ app/archive/a99/00001/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const a99_00001 = () => {
+  return (
+    <main>
+        Link: <a href="https://www.apa.org/monitor/2022/03/ce-corner-psychopathy">https://www.apa.org/monitor/2022/03/ce-corner-psychopathy </a>
+    </main>
+  )
+}
+
+export default a99_00001
